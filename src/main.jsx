831f6cff933bb4e263a5a543ef81a32b45d1c84f
humzasadiq <humzasadiq@gmail.com>
2024-06-14 03:17:40 +0500

    built ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import oneko from './services/neko.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import oneko from './services/neko.jsx'; // Correctly import the module
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
 
 oneko(); // Initialize the neko script
